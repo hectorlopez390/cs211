0759e0563e7d40a87a8e617c43571617507f1e20
Hector Lopez <hectorlopez.view@gmail.com>
2022-11-06 21:24:15 -0800

    Record output.txt

--- labs/lab_01/lab_01.cpp
+++ labs/lab_01/lab_01.cpp
@@ -1,2 +1,61 @@
 #include <iostream>
+#include <vector>
+#include <string>
+#include <fstream>
 using namespace std;
+
+int main()
+{
+  vector<string> V;
+  
+  string command;
+  string word;
+  int index;
+
+  ifstream fin;
+
+  fin.open("transaction_file.txt");
+
+  fin >> command;
+  
+  while(!fin.eof())
+    {
+      if (command == "Insert")
+	{
+	  fin >> word >> index;
+
+	  if (index == 0 || index < V.size())
+	    {
+	      V.insert(V.begin()+index, word);
+	    }
+	  else
+	    {
+	      cout << "Error: Insert not possible. Index beyond size of vector." << endl;
+	    }
+	}
+      else if (command == "Delete")
+	{
+	  fin >> index;
+
+	  if (index < V.size())
+	    {
+	      V.erase(V.begin()+index);
+	    }
+	  else
+	    {
+	      cout << "Error: Delete not possible. Index beyond size of vector." << endl;
+	    }
+	}
+      else if (command == "Print")
+	{
+	  for(int i = 0; i < V.size(); i++)
+	    {
+	      cout << V[i] << " ";
+	    }
+	  cout << endl;
+	}
+      fin >> command;
+    }
+  
+  return 0;
+}
