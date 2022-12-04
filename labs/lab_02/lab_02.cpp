084cd9a049900ce96bca5897cf2afc91fabc06ee
Hector Lopez <hectorlopez.view@gmail.com>
2022-12-04 13:06:41 -0800

    Write program to read student data and print student collection

--- labs/lab_02/lab_02.cpp
+++ labs/lab_02/lab_02.cpp
@@ -45,22 +45,65 @@ public:
 int main()
 {
   
-  StudentCollection collection;
+  StudentCollection collection; //create student collection object
 
-  StudentProfile new_entry;
+  StudentProfile new_entry; //create student profile object
 
   fstream fin;
   
   fin.open("data.txt");
   
-  /* test output
+  /* test input/output
 
      fin >> new_entry.PersonalInfo.SSN;
      
      collection.CS211.push_back(new_entry);
 
      cout << collection.CS211[0].PersonalInfo.SSN << endl;
-
   */
+
+  fin >> new_entry.PersonalInfo.SSN;
+  fin >> new_entry.PersonalInfo.Fname;
+  fin >> new_entry.PersonalInfo.Lname;
+  fin >> new_entry.PersonalInfo.Age;
+  fin >> new_entry.PersonalInfo.Gender;
+  fin >> new_entry.StdInfo.StNo;
+  fin >> new_entry.StdInfo.Course1.CourseNum;
+  fin >> new_entry.StdInfo.Course1.CourseName;
+  fin >> new_entry.StdInfo.Course2.CourseNum;
+  fin >> new_entry.StdInfo.Course2.CourseName;
+  fin >> new_entry.StdInfo.Course3.CourseNum;
+  fin >> new_entry.StdInfo.Course3.CourseName;
+
+  collection.CS211.push_back(new_entry);
+ 
+  while(!fin.eof())
+    {
+      fin >> new_entry.PersonalInfo.SSN;
+      fin >> new_entry.PersonalInfo.Fname;
+      fin >> new_entry.PersonalInfo.Lname;
+      fin >> new_entry.PersonalInfo.Age;
+      fin >> new_entry.PersonalInfo.Gender;
+      fin >> new_entry.StdInfo.StNo;
+      fin >> new_entry.StdInfo.Course1.CourseNum;
+      fin >> new_entry.StdInfo.Course1.CourseName;
+      fin >> new_entry.StdInfo.Course2.CourseNum;
+      fin >> new_entry.StdInfo.Course2.CourseName;
+      fin >> new_entry.StdInfo.Course3.CourseNum;
+      fin >> new_entry.StdInfo.Course3.CourseName;
+      
+      collection.CS211.push_back(new_entry);
+
+      cout << "New student added successfully." << endl;
+    }
+
+  cout << "Printing student collection..." << endl;
+
+  for(int i = 0; i < collection.CS211.size(); i++)
+    {
+
+      cout << collection.CS211[i].PersonalInfo.SSN << endl;
+    }
+  
   return 0;
 }
